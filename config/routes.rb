78a0fa5b876ca1001d972516ae984385ae77845c
shawn <shawn@naturalprofessional.com>
2017-03-01 13:29:21 -0800

    Add resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :brainjuices
+  resources :users do
+    resources :todos
+  end
   resources :todays
-  resources :todos
-
+  resources :brainjuices
 end
